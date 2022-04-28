1763e772c9a618ca67c5c46c8d2bbb2a684cd4a6
naresh2002kum <103481329+naresh2002kum@users.noreply.github.com>
2022-04-28 02:05:49 -0700

    commit-1

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const email= document.querySelector('#username');
+const email= document.querySelector('#Email');
 const pass = document.querySelector('#user-pin');
 const button = document.querySelector('#LoginBtn');
+const na = document.querySelector('#username');
 
 button.addEventListener('click',()=>{
+    if(na.value==''){
+        alert('Enter UserName')
+        return
+    }
     if(email.value==''){
-        alert('Username field is empty')
+        alert('Email field is empty')
         return
     }
     if(pass.value==''){
